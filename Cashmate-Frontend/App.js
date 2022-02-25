e4b596c0da9468e5f996ea30a977494eb9a5ccbe
Sharon Zou <43053716+szou00@users.noreply.github.com>
2022-02-24 20:59:02 -0500

    initialized react native project

---
+++ Cashmate-Frontend/App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Cashmate</Text>
+      <TextInput 
+        style={styles.input}
+        editable
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderColor: '#7a42f4',
+    borderWidth: 1,
+    width:200
+ },
+});
